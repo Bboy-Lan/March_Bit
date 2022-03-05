60f9ad91d31c989627a4b09959fce05b57f4d998
Bboy_Lan <948589783@qq.com>
2022-03-05 11:15:38 +0800

    一些更新

--- 比特C语言/Craft/main.c
+++ 比特C语言/Craft/main.c
@@ -2,6 +2,63 @@
 
 #include<stdio.h>
 
+
+// 这里自己注意 写连续不等式必须借助  &&-逻辑与
+int main()
+{
+    int sort;
+    scanf("%d", &sort);
+    if (90 < sort && sort <= 100)
+        printf("A\n");
+    else if (80 <= sort && sort < 90)
+        printf("B\n");
+    else if (70 <= sort && sort < 80)
+        printf("C\n");
+    else if (60 <= sort && sort < 70)
+        printf("D\n");
+    else if (0 <= sort && sort < 60)
+        printf("E\n");
+    else
+        printf("F\n");
+
+    return 0;
+}
+
+
+
+int main()
+{
+    int n;
+    int x;
+    int i = 0;
+    int arr[] = { 0 };
+    int sum = 0;
+
+    scanf("%d", &n);//输入一个数n
+
+    while (n > 0)//第二行输入n个空格隔开的数
+    {
+        scanf("%d ", &arr[n - 1]);
+        n--;
+    }
+    scanf("%d", &x);
+
+
+    for (i = 0; i < n; i++)//循环n次
+    {
+        if (arr[i] == x)//判断有是否相同的数
+        {
+            sum = sum + 1;
+        }
+    }
+    printf("%d\n", sum);//为啥最后输出是0 
+    return 0;
+}
+
+
+
+
+
 int main()
 {
     printf("printf(\"Hello world!\\n\");\n");
