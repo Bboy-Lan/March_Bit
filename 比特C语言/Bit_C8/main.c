b00aa15bc7451b7c5f8598f6126c50e538174243
Bboy_Lan <948589783@qq.com>
2022-03-09 18:07:11 +0800

    函数代码整理

--- 比特C语言/Bit_C8/main.c
+++ 比特C语言/Bit_C8/main.c
@@ -3,23 +3,264 @@
 #include<stdio.h>
 #include<string.h>
 #include<windows.h>
-#include<math.h>
+#include<math.h>//使用库函数 必须要引头文件
+#include<string.h>
 
 
-//1-100 之间所有3倍数的数
+// 函数 -库函数/自定义函数
+//函数可以嵌套调用 但不可以嵌套定义  因为无论是main还是其它函数都是并列的
+
+//所以才会有 函数定义在main函数之外的说法
+
+
+
 int main()
 {
-	int i;
-	
-	for (i = 3; i < 101; i += 3)
-	{
-		printf("%d ", i);
-	}
+	printf("%d", printf("%d", printf("%d", 43)));//为什么打印的是4321
+	//printf函数的返回值是其在屏幕上打印的字符的个数
+	//printf("%d", 43) 先打印43  两个字符 然后返回2
+	//printf("%d", printf("%d", 43)) == printf("%d", 2)  先打印2 然后返回1
+	//最后 printf("%d", 1)  这一步就直接打印 1
+
+
+	//而 scanf() 返回值是其成功读到的有效字符的个数
 	return 0;
 }
 
+//int main()
+//{
+//	//函数的链式访问-把一个函数的返回值直接作为另一个函数的参数
+//	int len = strlen("abc");
+//	printf("%d\n", len);
+//	printf("%d\n", strlen("abc"));
+//	// 比如这里 printf()函数 直接将strlen返回值作为参数
+//
+//	char arr1[20] = "######";
+//	char arr2[20] = "hello";
+//
+//
+//	printf("%s\n",strcpy(arr1,arr2));//这里返回值就是arr1
+//	return 0;
+//
+//}
+
+//写一个函数 使得每调用一次这个函数 num 增加 1
 
 
+//void ADD(int* a)
+//{
+//	*a += 1;
+//}
+//int main()
+//{
+//	int num = 0;
+//	ADD(&num);
+//	printf("%d\n", num);
+//	//题目给定的意思就是 我的函数有能力改变 main 函数内部的值 即传址调用(也可以普通方法)
+//	return 0;
+//}
+
+
+
+
+
+
+//写个函数 表示整形有序数组二分查找
+
+//int distinguish(int k,int arr[],int sz)//可以改成 传 左右
+//{
+//	int left=0;
+//	int right=sz-1;//函数这里并不是极简 还需要进一步简化
+//
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (k < arr[mid])
+//		{
+//			right = mid - 1;
+//		}
+//		else if (k > arr[mid])
+//		{
+//			left = mid + 1;
+//		}
+//		else
+//			return mid;
+//	}
+//	if (left > right)
+//		return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k=7;//需要查找下标的数
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	int num = distinguish(k,arr,sz);//这里注意预先需要传入函数的值
+//	if (num == 0)
+//		printf("没找到");
+//	else
+//	{
+//		printf("找到了: %d\n", num);
+//	}
+//	return 0;
+//}
+
+
+
+//写函数 -表示闰年
+
+//int is_year(int i)
+//{
+//	return ((i % 4 == 0) && (i % 100 != 0) || (i % 400 == 0));
+
+//}
+//int main()
+//{
+//	int i;
+//	int ret = 0;
+//	for (i = 1000; i <= 2000; i++)
+//	{
+//		if (is_year(i) == 1)
+//		{
+//			printf("%d ", i);
+//			ret++;
+//		}
+//	}
+//	printf("\n%d", ret);
+//	return 0;
+//}
+
+
+
+
+//int is_num(int a)//判断素数函数 试除法
+//{
+//	int j;
+//	for (j = 2; j < sqrt(a); j++)
+//	{
+//		if(a % j ==0)
+//			return 0;
+//	}
+//	return 1;
+//}
+//
+////写一个函数表示素数
+//int main()
+//{
+//	int i;
+//	int ret = 0;
+//	for (i = 101; i < 200; i += 2)
+//	{
+//		//判断是否为素数 一个函数-返回值
+//		if(is_num(i) == 1)
+//		{
+//			printf("%d ", i);
+//			ret++;
+//		}
+//	}
+//	printf("\n%d", ret);
+//	return 0;
+//}
+
+
+//形参在函数用完之后就会自动销毁
+
+//void swap(int* x, int* y)//无返回值
+//{
+//	int c = 0;
+//	c = *x;
+//	*x = *y;
+//	*y = c;
+//
+//}
+////调试的时候 逐步调试 F10\F11 的使用
+//int main()
+//{
+//	int a=10;
+//	int b = 20;
+//	printf("交换前：%d %d\n", a,b);
+//
+//	swap(&a,&b);
+//	//实参 a,b传给形式参数 x y 形参将会是实参的一份临时拷贝
+//	//且 改变形参的值 在函数内 是不会影响实参的
+//	printf("交换后：%d %d\n", a,b);
+//
+//	return 0;
+//}
+
+
+
+//int get_max(int x, int y)//形参
+//{
+//	return (x > y ? x : y);
+//}
+//int main()
+//{
+//	int a, b;
+//	scanf("%d%d", &a, &b);
+//
+//	int max = get_max(a, b);//实参
+//	printf("%d\n", max);
+//	return 0;
+//}
+
+
+
+
+
+
+/* memset example */
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char str[] = "almost every programmer should know memset!";
+//	memset(str, '-', 6);
+//	puts(str);
+//	return 0;
+//}
+
+
+//memset()
+
+
+//int main()
+//{
+//	char arr1[] = "hello_world";
+//
+//	memset(arr1, '4', 5);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+
+
+//strcpy()
+
+//int main()
+//{
+//	char arr1[] = "hello";
+//	char arr2[]="########";
+//
+//	char * tet=strcpy(arr2,arr1);//返回值是 指向的内存空间
+//	printf("%s\n", arr2);
+//	printf("%s\n", tet);// 并且随后的结束标志 " / 0"也跟着copy过去了
+//	
+//	return 0;
+//}
+
+
+//1-100 之间所有3倍数的数
+//int main()
+//{
+//	int i;
+//	
+//	for (i = 3; i < 101; i += 3)
+//	{
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
 
 
 
